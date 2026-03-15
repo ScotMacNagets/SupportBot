59c1d84276f74e4c9f70a44b67df376e519a6c97
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-15 19:38:26 +0300

    create database config

--- fastapi-supbot/core/config.py
+++ fastapi-supbot/core/config.py
@@ -1,16 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 class ApiPrefix(BaseModel):
     prefix: str = '/api'
 
-class RunConfig(BaseModel):
-    host: str = "0.0.0.0"
-    port: int = 8000
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
 
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
