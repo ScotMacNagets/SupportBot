eee8885eddbd18cb5b1cd5181bd79f9dbf52f578
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-16 11:31:48 +0300

    add tables to init.py

--- fastapi-supbot/core/models/__init__.py
+++ fastapi-supbot/core/models/__init__.py
@@ -1,5 +1,13 @@
 from .db_helper import db_helper
+from .base import Base
+from .user import User
+from .admin import Admin
+from .chat import Chat
 
 __all__ = (
     "db_helper",
+    "Base",
+    "User",
+    "Chat",
+    "Admin",
 )
