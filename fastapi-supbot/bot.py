b6a857aa0c4d18b3a312e5c30430d1cbbde4fa45
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-17 12:54:27 +0300

    create bot.py + connect handlers to dp router

---
+++ fastapi-supbot/bot.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+
+from aiogram import Dispatcher
+
+from core.bot_instance import bot
+from core.models import db_helper
+from handlers import admin_message_router, admin_register_router
+from middleware import DBMiddleware
+
+logger = logging.getLogger(__name__)
+
+#Bot
+dp = Dispatcher()
+dp.include_router(admin_register_router)
+dp.include_router(admin_message_router)
+
+dp.update.middleware(
+        DBMiddleware(
+            db=db_helper
+        )
+    )
+
+async def start_bot():
+    try:
+        await dp.start_polling(
+            bot,
+            skip_updates=True,
+        )
+    except (KeyboardInterrupt, asyncio.CancelledError):
+        logger.info("Got stop signal. Shutting down...")
+    except Exception as error:
+        logger.critical(
+            "Critical error while bot working: %s",
+            error,
+            exc_info=True,
+        )
+    finally:
+        await bot.session.close()
+        logger.info("Bot session closed.")
---
+++ fastapi-supbot/handlers/__init__.py
@@ -0,0 +1,7 @@
+from .admin_message import router as admin_message_router
+from .admin_register import router as admin_register_router
+
+__all__ = (
+    "admin_message_router",
+    "admin_register_router"
+)
