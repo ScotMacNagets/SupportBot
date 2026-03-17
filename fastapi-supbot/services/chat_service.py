eda7835727d9916efed94a4147dc3c6826284c97
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-17 12:57:51 +0300

    update logic in process_user_message

--- fastapi-supbot/services/chat_service.py
+++ fastapi-supbot/services/chat_service.py
@@ -1,7 +1,14 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+import logging
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from core.models import Chat
 from .chat_repo import ChatRepository
 from .message_repo import MessageRepository
+
+logger = logging.getLogger(__name__)
 
 
 class ChatService:
@@ -15,34 +22,38 @@ class ChatService:
         self.message_repo = MessageRepository(session)
 
     async def process_user_message(self, user_id: int, text: str):
-
         chat = await self.chat_repo.get_active_chat(user_id)
-
         if not chat:
             chat = await self.chat_repo.create_chat(user_id)
 
-        message = await self.message_repo.create_message(
-            chat_id=chat.id,
+        await self.message_repo.create_message(
             sender_id=user_id,
             sender_role="user",
             text=text,
+            chat_id=chat.id,
         )
 
-        if chat.admin_id:
-            await self.manager.send_to_admin(
-                admin_id=chat.admin_id,
-                chat_id=chat.id,
-                text=text
-            )
-        else:
+        chat = await self.session.execute(
+            select(Chat)
+            .options(selectinload(Chat.admin))  # сразу подгружаем admin
+            .where(Chat.status == "active", Chat.user_id == user_id)
+        )
+        chat = chat.scalars().first()
+
+        admin = chat.admin
+
+        if not admin:
             admin = await self.chat_repo.assign_free_admin(chat_id=chat.id)
 
-            if admin:
-                await self.manager.send_to_admin(
-                    admin_id=chat.admin_id,
-                    chat_id=chat.id,
-                    text=text,
-                )
+        if admin and admin.telegram_id:
+            await self.manager.send_to_admin(
+                admin_id=admin.telegram_id,
+                chat_id=chat.id,
+                text=text,
+                session=self.session,
+            )
+
+
     async def process_admin_message(
             self,
             admin_id: int,
@@ -50,8 +61,11 @@ class ChatService:
             text: str,
     ):
         chat = await self.chat_repo.get_chat(chat_id)
+        if not chat:
+            logger.info(f"Chat {chat_id} not found")
+            return
 
-        await self.message_repo.create_message(
+        message = await self.message_repo.create_message(
             chat_id=chat.id,
             sender_id=admin_id,
             sender_role="admin",
@@ -59,9 +73,6 @@ class ChatService:
         )
 
         await self.manager.send_to_user(
-            {
-                "type": "message",
-                "text": text
-            },
+            data=payload,
             user_id=chat.user_id,
         )
