8067936cecf414034d6969a442587925591878ac
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-17 12:54:43 +0300

    create main_app.py for fastapi aplication

---
+++ fastapi-supbot/main_app.py
@@ -0,0 +1,22 @@
+import logging
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from api import router as api_router
+from core.config import settings
+from core.models import db_helper
+
+logger = logging.getLogger(__name__)
+
+#FastAPI
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Lifespan events started")
+    yield
+    db_helper.dispose()
+    logger.info("Lifespan events started")
+main_app = FastAPI(
+    lifespan=lifespan,
+)
+main_app.include_router(api_router, prefix=settings.api.prefix)
