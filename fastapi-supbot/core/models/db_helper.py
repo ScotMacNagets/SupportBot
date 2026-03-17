6de4150455465fef1d28478d3347f31ab56a6d06
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-17 12:43:38 +0300

    add space between func in db_helper.py

--- fastapi-supbot/core/models/db_helper.py
+++ fastapi-supbot/core/models/db_helper.py
@@ -32,6 +32,7 @@ class DatabaseHelper:
     async def dispose(self) -> None:
         await self.engine.dispose()
 
+
     async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
             yield session
