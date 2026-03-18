e510a9ec0ad2de040664387a45314c6520ec0ea0
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-18 16:07:51 +0300

    create get_active_connections + add loge in connections_manager.py

--- fastapi-supbot/core/connection_manager.py
+++ fastapi-supbot/core/connection_manager.py
@@ -1,69 +1,52 @@
+import logging
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
-# from core.bot_instance import bot
-# from core.models import Message
-#
-#
-# async def send_to_admin(admin_id: int, chat_id: int, text: str):
-#
-#     message = (
-#         f"ChatID: {chat_id}\n\n",
-#         f"Message: \n{text}",
-#     )
-#
-#     await bot.send_message(admin_id, message)
+from core.bot_instance import bot
+from core.models import Message
+
+logger = logging.getLogger(__name__)
 
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[int, WebSocket] = {}
 
+
     async def connect(self, user_id: int, websocket: WebSocket):
         await websocket.accept()
 
         self.active_connections[user_id] = websocket
 
-    def disconnect(self, user_id: int):
+
+
+    async def disconnect(self, user_id: int):
         if user_id in self.active_connections:
             del self.active_connections[user_id]
 
-    async def send_to_user(self, user_id: int, data: dict):
-        websocket = self.active_connections[user_id]
 
-        if websocket:
+    def get_active_connections(self, user_id: int):
+        websocket = self.active_connections.get(user_id)
+        if not websocket:
+            return False
+        return websocket
+
+
+    async def send_to_user(self, user_id: int, data: dict) -> bool:
+        websocket = self.active_connections.get(user_id)
+
+        if not websocket:
+            logger.info("User %s has no active connection", user_id)
+            return False
+
+        try:
             await websocket.send_json(data)
-
-    @staticmethod
-    async def send_to_admin(
-            admin_id: int,
-            chat_id: int,
-            text: str,
-            session: AsyncSession,
-    ):
-
-        message = f"Message: \n\n{text}"
-
-        msg = await bot.send_message(admin_id, message)
-        telegram_message_id = msg.message_id
-
-        query = (
-            select(Message)
-            .where(
-                Message.chat_id == chat_id,
-                Message.telegram_message_id.is_(None),
-            )
-            .order_by(Message.id.desc())
-        )
-        result = await session.execute(query)
-        msg_model = result.scalars().first()
-        if msg_model:
-            msg_model.telegram_message_id = telegram_message_id
-            await session.commit()
-        
-
-
+            return True
+        except (WebSocketDisconnect, RuntimeError) as e:
+            logger.warning(f"Failed to send message to %s: %s", user_id, e)
+            return False
 
 
 manager = ConnectionManager()
