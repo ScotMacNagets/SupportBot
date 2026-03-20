81cf00530a0bf2d5b627e65557cc3466087f9290
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-20 13:33:59 +0300

    Create message handler: now admin can accept chat and write directly to the user without replies

--- fastapi-supbot/handlers/admin_message.py
+++ fastapi-supbot/handlers/admin_message.py
@@ -132,13 +132,6 @@ async def admin_answer(
 
     admin.current_chat_id = None
 
-    async for session in db_helper.session_getter():
-        admin = await session.scalar(
-            select(Admin).where(Admin.telegram_id == message.from_user.id)
-        )
-        if not admin:
-            await message.reply("Ты не зарегистрирован, как админ в системе")
-            return
     await session.commit()
 
     await query.answer()
@@ -146,25 +139,39 @@ async def admin_answer(
         text="✅ Диалог успешно завершен",
     )
 
+@router.message()
+async def admin_answer(
+        message: TelegramMessage,
+        session: AsyncSession,
+):
+    admin = await session.scalar(
+        select(Admin).where(Admin.telegram_id == message.from_user.id)
+    )
 
-        src_message = await session.scalar(
-            select(Message).where(Message.telegram_message_id == replied_msg_id)
+    if not admin:
+        return
+
+    if not admin.current_chat_id:
+        await message.answer(
+            text="⛔ У тебя нет активного диалога"
         )
-        if not src_message:
-            await message.reply(
-                "Не смог найти, к какому сообщению относится твой ответ. "
-                "Убедись, что отвечаешь именно на сообщение бота с текстом пользователя."
-            )
-            return
-        service = ChatService(
-            session=session,
-            manager=manager,
+        return
+
+    chat = await session.get(Chat, admin.current_chat_id)
+
+    if chat.status != "active":
+        await message.answer(
+            text="🗂 Диалог уже закрыт",
         )
+        return
 
-        await service.process_admin_message(
-            admin_id=admin.id,
-            chat_id=src_message.chat_id,
-            text=message.text,
-        )
+    service = ChatService(
+        session=session,
+        manager=manager,
+    )
 
-
+    await service.process_admin_message(
+        admin_id=admin.id,
+        chat_id=chat.id,
+        text=message.text,
+    )
