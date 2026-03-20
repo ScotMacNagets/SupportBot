d1eaf73f0ea66306d18b7bd7b99bc9de6e75b3de
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-20 13:29:48 +0300

    Commented old way of answering the user message

--- fastapi-supbot/handlers/admin_message.py
+++ fastapi-supbot/handlers/admin_message.py
@@ -17,6 +17,48 @@ logger = logging.getLogger(__name__)
 async def admin_reply(
         message: TelegramMessage,
         session: Session,
+# @router.message()
+# async def admin_reply(
+#         message: TelegramMessage,
+#         session: AsyncSession,
+# ):
+#
+#     if not message.reply_to_message:
+#         await message.reply(
+#             text="Ответь реплаем на сообщение пользователя, чтобы отправить ответ в чат."
+#         )
+#         return
+#
+#     telegram_message_id = message.reply_to_message.message_id
+#
+#
+#     admin = await session.scalar(
+#         select(Admin).where(Admin.telegram_id == message.from_user.id)
+#     )
+#     if not admin:
+#         await message.reply("Ты не зарегистрирован, как админ в системе")
+#         return
+#
+#     src_message = await session.scalar(
+#         select(Message).where(Message.telegram_message_id == telegram_message_id)
+#     )
+#     if not src_message:
+#         await message.reply(
+#             "Не смог найти, к какому сообщению относится твой ответ. "
+#             "Убедись, что отвечаешь именно на сообщение бота с текстом пользователя."
+#         )
+#         return
+#     service = ChatService(
+#         session=session,
+#         manager=manager,
+#     )
+#
+#     await service.process_admin_message(
+#         admin_id=admin.id,
+#         chat_id=src_message.chat_id,
+#         text=message.text,
+#     )
+
 ):
 
     if not message.reply_to_message:
