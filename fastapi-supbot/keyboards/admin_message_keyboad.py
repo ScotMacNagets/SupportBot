9f883448da60f207ab0dc1473d5c97e90d5d654d
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-20 21:32:30 +0300

    Update text in keyboard builder

--- fastapi-supbot/keyboards/admin_message_keyboad.py
+++ fastapi-supbot/keyboards/admin_message_keyboad.py
@@ -2,15 +2,15 @@ from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from callbacks.admin_message_callback import Action, AdminAction
+from core.text import AdminMessageKeyboard
 
 
-
-async def answer_keyboard(chat_id: int, first_message: bool) -> InlineKeyboardMarkup:
+async def answer_keyboard(chat_id: int) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
 
     builder.button(
-        text="✅ Принять",
+        text=AdminMessageKeyboard.ACCEPT,
         callback_data=AdminAction(
             action=Action.answer,
             chat_id=chat_id,
@@ -18,7 +18,7 @@ async def answer_keyboard(chat_id: int, first_message: bool) -> InlineKeyboardMa
     )
 
     builder.button(
-            text="⛔ Закрыть",
+            text=AdminMessageKeyboard.CLOSE,
             callback_data=AdminAction(
                 action=Action.close,
                 chat_id=chat_id,
