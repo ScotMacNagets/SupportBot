33ff9bab28da7788ef894121ae963146a428a402
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-22 19:39:55 +0300

    Include superuser_menu_router to the main router

--- fastapi-supbot/handlers/__init__.py
+++ fastapi-supbot/handlers/__init__.py
@@ -1,7 +1,9 @@
 from .admin_message import router as admin_message_router
 from .admin_register import router as admin_register_router
+from .superuser_menu import router as superuser_menu_router
 
 __all__ = (
     "admin_message_router",
-    "admin_register_router"
+    "admin_register_router",
+    "superuser_menu_router",
 )
