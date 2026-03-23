647d8831b49312ff12bd5edc061aacdc331621d7
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-23 21:24:00 +0300

    Rename cb in super_user_menu_callback.py

--- fastapi-supbot/callbacks/super_user_menu_callback.py
+++ fastapi-supbot/callbacks/super_user_menu_callback.py
@@ -5,16 +5,20 @@ from aiogram.filters.callback_data import CallbackData
 from core.config import settings
 
 
-class Action(str, Enum):
+class SuperuserAction(str, Enum):
     realise_new_key = "realise_new_key"
     admin_list = "admin_list"
+    back_to_the_main_menu = "back_to_the_main_menu"
+
+class SuperuserDeleteActions(str, Enum):
     admin_delete = "admin_delete"
     confirm_delete = "confirm_delete"
 
-class SuperuserMenuAction(CallbackData, prefix="supuser"):
-    action: Action
+class SuperuserMenu(CallbackData, prefix=settings.cb.superuser_menu.prefix):
+    action: SuperuserAction
 
-class AdminDeleteAction(CallbackData, prefix="admindelete"):
-    action: Action
-    telegram_id: str
+class SuperuserAdminDelete(CallbackData, prefix=settings.cb.superuser_admin_delete.prefix):
+    action: SuperuserDeleteActions
+    telegram_id: int
+
 
