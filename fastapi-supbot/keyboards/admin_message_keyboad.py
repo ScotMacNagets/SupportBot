1996c3f0230b1cfe2ba67958102a6117b7006185
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-23 21:19:48 +0300

    little fixes in admin_message_keyboad.py

--- fastapi-supbot/keyboards/admin_message_keyboad.py
+++ fastapi-supbot/keyboards/admin_message_keyboad.py
@@ -8,21 +8,21 @@ from core.text import AdminMessageKeyboard
 async def answer_keyboard(chat_id: int) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
-
     builder.button(
         text=AdminMessageKeyboard.ACCEPT,
         callback_data=AdminAction(
             action=Action.answer,
             chat_id=chat_id,
         ).pack()
+
     )
 
     builder.button(
-            text=AdminMessageKeyboard.CLOSE,
-            callback_data=AdminAction(
-                action=Action.close,
-                chat_id=chat_id,
-            ).pack()
-        )
+        text=AdminMessageKeyboard.CLOSE,
+        callback_data=AdminAction(
+            action=Action.close,
+            chat_id=chat_id,
+        ).pack()
+    )
 
     return builder.as_markup()
