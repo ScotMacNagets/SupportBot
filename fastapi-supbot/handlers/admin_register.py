960f823ee9a97f4884eb0c70844c840b9ecd51e9
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-23 21:21:22 +0300

    add logger + exception to the admin_register.py + move all text to the text.py

--- fastapi-supbot/handlers/admin_register.py
+++ fastapi-supbot/handlers/admin_register.py
@@ -1,15 +1,21 @@
+import logging
+
 from aiogram import Router
 from aiogram.enums import ParseMode
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 
 from aiogram.types import Message
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.config import settings
 from core.models import Admin
 from core.text import AdminRegister
 from fsm.admin_registration import Registration
 from services import key_repo
+
+logger = logging.getLogger(__name__)
 
 router = Router()
 
@@ -32,12 +38,12 @@ async def _admin_register_helper(
 async def admin_register(
         message: Message,
         state: FSMContext,
-        session: AsyncSession,
 ):
     #Начало регистрации, просим ввести уникальный ключ
     await state.set_state(Registration.input_key)
     await message.answer(
-        text="Введите уникальный ключ для регистрации"
+        text=AdminRegister.INPUT_REGISTER_KEY,
+        parse_mode=ParseMode.HTML,
     )
 
 @router.message(Registration.input_key)
@@ -57,11 +63,19 @@ async def admin_register_check_key(
         #заполняем данные админа
         username = message.from_user.username
         telegram_id = message.from_user.id
-        await _admin_register_helper(
-            username=username,
-            telegram_id=telegram_id,
-            session=session,
-        )
+        try:
+            await _admin_register_helper(
+                username=username,
+                telegram_id=telegram_id,
+                session=session,
+            )
+        except IntegrityError as e:
+            logger.info("Unsuccessful registration: admin already registered, %s", e)
+            await message.answer(
+                text=AdminRegister.ALREADY_REGISTERED
+            )
+            return
+        logger.info("Successful registration: %s", username)
         await message.answer(
             text=AdminRegister.SUCCESSFUL_REGISTRATION.format(
                 username=username,
@@ -70,10 +84,11 @@ async def admin_register_check_key(
         )
 
         return
+    logger.warning("Unsuccessful registration: %s", message.from_user.username)
     await message.answer(
-        text="⚠️ Не удалось вас зарегестрировать.\n\n"
-        "Обратитесь к <a href='https://t.me/{admin_username}'>администратору</a> " 
-        "для получения доступа.",
+        text=AdminRegister.UNSUCCESSFUL_REGISTRATION.format(
+            username=settings.superuser.username,
+        ),
         parse_mode=ParseMode.HTML,
     )
 
