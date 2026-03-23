f92bc2945829826bd2dfa44460ccad234e461152
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-03-23 15:44:18 +0300

    add chat status in answer_keyboard so the inline buttons appearing depends on chat status

--- fastapi-supbot/keyboards/admin_message_keyboad.py
+++ fastapi-supbot/keyboards/admin_message_keyboad.py
@@ -2,22 +2,32 @@ from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from callbacks.admin_message_callback import Action, AdminAction
+from core.config import settings
 from core.text import AdminMessageKeyboard
 
 
-async def answer_keyboard(chat_id: int) -> InlineKeyboardMarkup:
+async def answer_keyboard(chat_id: int, chat_status: str) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
+    if chat_status == settings.chat_states.new:
+        builder.button(
+            text=AdminMessageKeyboard.ACCEPT,
+            callback_data=AdminAction(
+                action=Action.answer,
+                chat_id=chat_id,
+            ).pack()
+        )
 
-    builder.button(
-        text=AdminMessageKeyboard.ACCEPT,
-        callback_data=AdminAction(
-            action=Action.answer,
-            chat_id=chat_id,
-        ).pack()
-    )
+        builder.button(
+            text=AdminMessageKeyboard.CLOSE,
+            callback_data=AdminAction(
+                action=Action.close,
+                chat_id=chat_id,
+            ).pack()
+        )
 
-    builder.button(
+    elif chat_status == settings.chat_states.active:
+        builder.button(
             text=AdminMessageKeyboard.CLOSE,
             callback_data=AdminAction(
                 action=Action.close,
